51feade82f0b12bdf4389017e099d1b44effd477
Dimitrije Pavlov <dpavlov@college.harvard.edu>
2022-11-27 21:52:28 -0500

    Don't forget main

--- src/main.cc
+++ src/main.cc
@@ -3,8 +3,8 @@
 #include <iostream>
 
 int main() {
-    Simulator s(4);
-    s.generate_data(1000000, 0.999);
+    Simulator s(2, 2048);
+    s.generate_data(134217728, 0.999);
     s.run();
     std::cout << "Done" << std::endl;
 }
